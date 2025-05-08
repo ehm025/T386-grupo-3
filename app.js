7250df8a73c8beec6388fbf4c4479b2f3f67dcfe
ehm025 <23016363+ehm025@users.noreply.github.com>
2025-05-08 01:24:02 -0600

    Actualizado el app.js y agregado el README con instrucciones

---
+++ app.js
@@ -0,0 +1,7 @@
+const factorial = require('./calculo-de-factorial');
+const fibonacci = require('./calculo-fibonacci');
+const primos = require('./verificacion-numeros-primos');
+
+console.log('Factorial: ', factorial(5));
+console.log('Fibonacci: ', fibonacci(5));
+console.log('Numero primo: ', primos(11));
